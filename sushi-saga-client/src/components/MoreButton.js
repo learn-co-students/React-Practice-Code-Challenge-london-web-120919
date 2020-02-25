0276839f50fea198a76512e982a87b8e67ba7a30
sarafotros <sarafotros@gmail.com>
2020-02-25 11:52:08 +0000

    Done.

--- sushi-saga-client/src/components/MoreButton.js
+++ sushi-saga-client/src/components/MoreButton.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const MoreButton = (props) => {
-    return <button onClick={/* Fill me in! */ null}>
-            More sushi!
-          </button>
-}
+const MoreButton = ({ increaseSushiIndex }) => {
+	return <button onClick={increaseSushiIndex}>More sushi!</button>;
+};
 
 export default MoreButton
